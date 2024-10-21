9808e7dd4713835077c70a7aec828bc32e0ae2e0
Vien Galang <viengalang@Viens-MacBook-Air.local>
2024-10-21 11:04:12 -0700

    included bonus part

--- question2.js
+++ question2.js
@@ -19,7 +19,7 @@ CHALLENGE (SAVE FOR LAST):
 */
 
 /** PLANNING
- * Scenario: Midterm exam grading system
+ * Scenario: Midterm exam grading system. Instructor will input the student's name and midterms exam score to the grading system
  * 
  * Enter the student name to input1
  * Enter student's score to input 2
@@ -29,6 +29,9 @@ CHALLENGE (SAVE FOR LAST):
  * if score is >=60 and <=71 Good effort! Student has a C
  * if score is >=50 and <=59 Needs improvement! Student has a D
  * if score is <50, prompt FAIL! Student has an F!
+ * 
+ * if score -2 from the higher rank, prompt almost got the higher rank
+ * if score is +2 from the lower rank, prompt almost falling to next lower rank
  */
 
 //determine a proper function name and parameter variable name
@@ -36,16 +39,38 @@ function studentGrade(paramVariable1, paramVariable2){
   let sName = paramVariable1;
   let score = paramVariable2;
 
+
   if (score >= 86 ) {
     console.log("Excellent! ", sName, " has an A!");
+    if (score === 86 || score === 87) {
+      console.log("Work harder. ", sName ,"Almost fall to B.");
+    }
   } else if (score >= 72 && score <= 85 ) {
     console.log("Great job!! ", sName, " has a B!");
+    if (score === 85 || score === 84){
+      console.log("Almost there! ", sName,  " is close to A!");
+    } else if (score === 72 || score === 73) {
+      console.log("Work harder. ", sName ,"Almost fall to C.");
+    }
   } else if (score >= 60 && score <= 71 ) {
     console.log("Good effort!! ", sName, " has a C!");
+    if (score === 71 || score === 70){
+      console.log("Almost there! ", sName,  " is close to B!");
+    } else if (score === 60 || score === 61) {
+      console.log("Work harder. ", sName ,"Almost fall to D.");
+    }
   } else if (score >= 50 && score <= 59 ) {
     console.log("Needs improvement!! ", sName, " has a D!");
+    if (score === 59 || score === 58){
+      console.log("Almost there! ", sName,  " is close to C!");
+    } else if (score === 50 || score === 51) {
+      console.log("Work harder. ", sName ,"Almost fall to E.");
+    }
   } else if (score < 50) {
     console.log("FAIL!! ", sName, " has a E!");
+    if (score === 49 || score === 48){
+      console.log("Almost there! ", sName,  " is close to D!");
+    }
   }
   //write your code here
 }
